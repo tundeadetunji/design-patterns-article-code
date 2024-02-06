997a73bb3f3898886c14ea5ef79cd9abc9aa360c
Tunde Adetunji <tundeadetunji2017@gmail.com>
2024-02-06 16:05:31 +0100

    Release Template

--- src/main/java/io/github/tundeadetunji/regsys/Start.java
+++
@@ -1,9 +0,0 @@
-package io.github.tundeadetunji.regsys;
-
-import io.github.tundeadetunji.regsys.template.EmailProcessorTemplate;
-import io.github.tundeadetunji.regsys.noun.Protocol;
-
-public class Start {
-    public static void main(String[] args) {
-    }
-}
